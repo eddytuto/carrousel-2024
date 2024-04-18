6406d4566611a57319180e55cab764c04ddd7108
Eddy Martin <78427613+eddytuto@users.noreply.github.com>
2024-04-18 12:03:34 -0400

    s10c2 début intégration des images de galerie vers le carrousel

--- js/carrousel.js
+++ js/carrousel.js
@@ -6,6 +6,37 @@
     console.log("bouton = " + bouton.tagName)
     let carrousel__x = document.querySelector('.carrousel__x')
     console.log('carrousel__x' + carrousel__x.tagName)
+    let galerie = document.querySelector('.galerie')
+    console.log('galerie = ' + galerie.tagName)
+
+
+    let carrousel__figure = document.querySelector('.carrousel__figure')
+    /* Création dynamique d'une image du carrousel */
+    let carrousel__img = document.createElement('img')
+    carrousel__img.classList.add('carrousel__img')
+    /* récupère la première image de la galerie */
+    // let galerie__img = galerie.querySelector('img')
+    /* por créer une collection d'images de la galerie */
+    let galerie__img = galerie.querySelectorAll('img')
+    console.log( galerie__img)
+    for (const elm of galerie__img)
+    {
+        console.log(elm.src)
+    }
+
+    /*
+    console.log("première image de la galerie = " + galerie__img.src)
+    carrousel__img.src = galerie__img.src
+    console.log("première image du carrousel = " + carrousel__img.src)
+    carrousel__figure.appendChild(carrousel__img)
+    console.log(carrousel__figure)
+*/
+
+
+
+
+
+
 
 /* écouteur pour ouvrir la boîte modale */
     bouton.addEventListener('mousedown', function(){
@@ -15,5 +46,7 @@
     carrousel__x.addEventListener('mousedown', function(){
         carrousel.classList.remove('carrousel--ouvrir') // fermer le carrousel
     })
+
+
 
 })()
